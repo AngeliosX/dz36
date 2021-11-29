84047e60a0e02cf6ee5772c4453a8f33eab329c2
Angelios <angelionus@gmail.com>
2021-11-29 11:55:21 +0500

    End of the project NIO-GIT

--- src/dz36/Main.java
+++ src/dz36/Main.java
@@ -1,8 +1,22 @@
 package dz36;
 
-public class Main {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("start");
+        Path filePath = Paths.get("file.txt");
+        try {
+            Files.write(filePath, "Привет nio".getBytes(UTF_8));
+            byte[] bytes = Files.readAllBytes(filePath);
+            System.out.println(new String(bytes, UTF_8));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
